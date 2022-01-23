8ddde7bbc8f37296cd3208294f62139a845630e7
AbhAsg09 <abhishekasgola09@gmail.com>
2022-01-23 15:28:49 +0530

    Initial commit

---
+++ src/Abhishek/Palindrome_String.java
@@ -0,0 +1,28 @@
+package Abhishek;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Palindrome_String {
+    static boolean checkPalindrome(String word){
+        word = word.toLowerCase(Locale.ROOT);
+        StringBuilder newWord = new StringBuilder();
+        char letter;
+        for (int i = 0; i < word.length(); i++) {
+            letter = word.charAt(i);
+            newWord.insert(0, letter);
+        }
+        return newWord.toString().equals(word);
+    }
+    public static void main(String[] args) {
+        Scanner news = new Scanner(System.in);
+        String word;
+        System.out.println("Enter word");
+        word = news.next();
+        if (checkPalindrome(word)){
+            System.out.println("String is palindrome");
+        } else {
+            System.out.println("String is not palindrome");
+        }
+    }
+}
